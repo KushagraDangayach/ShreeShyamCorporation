11f4291cfb461efd95bcb24f0e3ec4189554ba99
KushagraDangayach <kushagra.dangayach01@gmail.com>
2025-02-14 20:48:35 +0530

    changed font of header to make responsive

--- src/compo/Header.jsx
+++ src/compo/Header.jsx
@@ -5,12 +5,25 @@ const Header = () => {
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-black">
           <div className="flex items-center gap-4">
-            <span className="flex items-center">
-              <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+            <span className="flex items-center text-base sm:text-sm md:text-base">
+              <svg
+                className="w-4 h-4 mr-2 sm:w-3 sm:h-3"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                />
               </svg>
-              support@shrishyamcorporation.in
+              <span className="text-sm sm:text-xs md:text-sm">
+                support@shrishyamcorporation.in
+              </span>
             </span>
+
             <span className="flex items-center">
               <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
